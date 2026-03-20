92a20e84747548a54d249530b5daeb6d66996142
BHATTJI4U <pb220979@gmail.com>
2026-03-20 13:24:38 +0530

    first upload

---
+++ final.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import fitz
+import base64
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    pages = []
+
+    if request.method == "POST":
+        file = request.files["pdf"]
+
+        if file:
+            doc = fitz.open(stream=file.read(), filetype="pdf")
+
+            for page in doc:
+                pix = page.get_pixmap()
+                img_bytes = pix.tobytes("png")
+                img_base64 = base64.b64encode(img_bytes).decode("utf-8")
+                pages.append(img_base64)
+
+    return render_template("FINAL JEE.html", pages=pages)
+
+if __name__ == "__main__":
+    app.run(debug=True)
